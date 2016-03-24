a37f3d012f6430ed1897197b9c15c9899bb61eca
IgorHero <belyanchikovia@gmail.com>
2016-03-25 00:49:01 +0400

    some edit

--- FindPair/FindPair/FindPair/Form1.Designer.cs
+++ FindPair/FindPair/FindPair/Form1.Designer.cs
@@ -28,27 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.NewGameButton = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ScoreTimer = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Enabled = false;
             this.pictureBox1.Location = new System.Drawing.Point(13, 13);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(360, 450);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
             // 
             // NewGameButton
@@ -59,7 +61,7 @@
             this.NewGameButton.TabIndex = 1;
             this.NewGameButton.Text = "New game";
             this.NewGameButton.UseVisualStyleBackColor = true;
-            this.NewGameButton.Click += new System.EventHandler(this.button1_Click);
+            this.NewGameButton.Click += new System.EventHandler(this.NewGameButton_Click);
             // 
             // groupBox1
             // 
@@ -73,6 +75,26 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Size";
             // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(7, 66);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(48, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "5 x 4";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(7, 43);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(48, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "4 x 4";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
@@ -85,60 +107,31 @@
             this.radioButton1.Text = "3 x 4";
             this.radioButton1.UseVisualStyleBackColor = true;
             // 
-            // radioButton2
+            // timer1
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(7, 43);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(48, 17);
-            this.radioButton2.TabIndex = 1;
-            this.radioButton2.Text = "4 x 4";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // radioButton3
+            // ScoreTimer
             // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(7, 66);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(48, 17);
-            this.radioButton3.TabIndex = 2;
-            this.radioButton3.Text = "5 x 4";
-            this.radioButton3.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(386, 264);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 13);
-            this.label1.TabIndex = 3;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(386, 316);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 13);
-            this.label2.TabIndex = 4;
+            this.ScoreTimer.Interval = 500;
+            this.ScoreTimer.Tick += new System.EventHandler(this.ScoreTimer_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 482);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(484, 492);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.NewGameButton);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Find pair";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -150,8 +143,8 @@
         private System.Windows.Forms.RadioButton radioButton3;
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer ScoreTimer;
     }
 }
 
---
+++ FindPair/FindPair/FindPair/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FindPair
+{
+    public partial class Form1 : Form
+    {
+        Bitmap PictureA = new Bitmap(Properties.Resources.А);
+        Bitmap[] image1 = new Bitmap[11];
+        int step = 0;
+        int[,] Mas = new int[4, 5];
+        int razm = 0, CurX = -1, CurY = -1, PrevX = -1, PrevY = -1, Score, turn, TimeScore = 100;
+        bool flag;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Graphics Graph = pictureBox1.CreateGraphics();
+            turn++;
+            if (turn == 5)
+            {
+                for (int i = 0; i < razm; i++)
+                    for (int j = 0; j < 4; j++)
+                        Graph.DrawImage(image1[10], j * 90, i * 90);
+                pictureBox1.Enabled = true;
+                timer1.Enabled = false;
+                ScoreTimer.Enabled = true;
+                turn = 0;
+            }
+        }
+
+        public void SwitchBlockControl()
+        {
+            groupBox1.Enabled = !groupBox1.Enabled;
+            NewGameButton.Enabled = !NewGameButton.Enabled;
+        }
+
+        private void NewGameButton_Click(object sender, EventArgs e)
+        {
+
+            timer1.Enabled = true;
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 5; j++)
+                    Mas[i, j] = -1;
+            SwitchBlockControl();
+            if (radioButton1.Checked)
+                razm = 3;
+            else if (radioButton2.Checked)
+                razm = 4;
+            else
+                razm = 5;
+            TimeScore = 50 * razm;
+            Draw();
+        }
+
+        public void CheckWin()
+        {
+            if (Score == razm * 2)
+            {
+                ScoreTimer.Enabled = false;
+                MessageBox.Show("win " + TimeScore);
+                pictureBox1.Enabled = false;
+                NewGameButton.Enabled = true;
+                groupBox1.Enabled = true;
+                Score = 0;
+                CurX = -1;
+                CurY = -1;
+                PrevX = -1;
+                PrevY = -1;
+            }
+        }
+
+        public void Draw()
+        {
+            int x = 0, y = 0;
+            Random rnd1 = new Random();
+            for (int i = 0; i < razm * 4; i++)
+            {
+                do
+                {
+                    x = rnd1.Next(0, 4);
+                    y = rnd1.Next(0, razm);
+                } while (Mas[x, y] != -1);
+                Mas[x, y] = i / 2;
+
+            }
+            Graphics Graph = pictureBox1.CreateGraphics();
+            for (int i = 0; i < razm; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Graph.DrawImage(image1[Mas[j, i]], j * 90, i * 90);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            image1[0] = Properties.Resources.А;
+            image1[1] = Properties.Resources.Б;
+            image1[2] = Properties.Resources.В;
+            image1[3] = Properties.Resources.Г;
+            image1[4] = Properties.Resources.Д;
+            image1[5] = Properties.Resources.Е;
+            image1[6] = Properties.Resources.Ж;
+            image1[7] = Properties.Resources.З;
+            image1[8] = Properties.Resources.И;
+            image1[9] = Properties.Resources.К;
+            image1[10] = Properties.Resources.Clear;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Graphics Graph = pictureBox1.CreateGraphics();
+            int x = e.X / 90;
+            int y = e.Y / 90;
+            if ((Mas[x, y] != -1) && ((CurX != x) || (CurY != y)))
+            {
+                if ((step == 0) && ((PrevX != x) || (PrevY != y)))
+                {
+                    if (!flag)
+                    {
+                        Graph.DrawImage(image1[10], CurX * 90, CurY * 90);
+                        Graph.DrawImage(image1[10], PrevX * 90, PrevY * 90);
+                    }
+                    CurX = x;
+                    CurY = y;
+                    Graph.DrawImage(image1[Mas[CurX, CurY]], CurX * 90, CurY * 90);
+                    step = 1;
+                }
+                else if ((step == 1) && ((CurX != x) || (CurY != y)))
+                {
+                    PrevX = CurX;
+                    PrevY = CurY;
+                    CurX = x;
+                    CurY = y;
+                    Graph.DrawImage(image1[Mas[CurX, CurY]], CurX * 90, CurY * 90);
+                    step = 0;
+                    if (Mas[CurX, CurY] == Mas[PrevX, PrevY])
+                    {
+                        Score++;
+
+                        flag = true;
+                        Mas[CurX, CurY] = -1;
+                        Mas[PrevX, PrevY] = -1;
+                        CheckWin();
+                    }
+                    else
+                        flag = false;
+                }
+            }
+        }
+
+        private void ScoreTimer_Tick(object sender, EventArgs e)
+        {
+            TimeScore--;
+        }
+    }
+}
