5dbc9406832e1ebc844e8eff3136cf25c85eaae1
IgorHero <belyanchikovia@gmail.com>
2016-03-25 00:29:57 +0400

    Begin

---
+++ FindPair/FindPair/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace FindPair
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.NewGameButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(360, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // NewGameButton
+            // 
+            this.NewGameButton.Location = new System.Drawing.Point(389, 214);
+            this.NewGameButton.Name = "NewGameButton";
+            this.NewGameButton.Size = new System.Drawing.Size(75, 23);
+            this.NewGameButton.TabIndex = 1;
+            this.NewGameButton.Text = "New game";
+            this.NewGameButton.UseVisualStyleBackColor = true;
+            this.NewGameButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(403, 76);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(61, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Size";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 20);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(48, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "3 x 4";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(7, 43);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(48, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "4 x 4";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(7, 66);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(48, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "5 x 4";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(386, 264);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(386, 316);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 482);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.NewGameButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button NewGameButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FindPair/FindPair/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FindPair
+{
+    public partial class Form1 : Form
+    {
+        Image[] imag = new Image[10];
+        Bitmap PictureA = new Bitmap(Properties.Resources.А);
+        Bitmap[] image1 = new Bitmap[10];
+        int step = 0;
+        int[,] Mas = new int[4, 5];
+        int razm = 0, CurI, PrevI;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void SwitchBlockControl()
+        {
+            groupBox1.Enabled = !groupBox1.Enabled;
+            NewGameButton.Enabled = !NewGameButton.Enabled;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 5; j++)
+                    Mas[i, j] = -1;
+            SwitchBlockControl();
+            if (radioButton1.Checked)
+                razm = 3;
+            else if (radioButton2.Checked)
+                razm = 4;
+            else
+                razm = 5;
+            Draw();
+        }
+
+        public void Draw()
+        {
+            int x = 0, y = 0;
+            Random rnd1 = new Random();
+            label1.Text += "x y\n";
+            for (int i = 0; i < razm * 2; i++)
+            {
+                do
+                {
+                    x = rnd1.Next(0, 4);
+                    y = rnd1.Next(0, razm);
+                } while (Mas[x, y] != -1);
+                Mas[x, y] = i;
+                // Graph.DrawImage(image1[i], x * 90, y * 90);
+                do
+                {
+                    x = rnd1.Next(0, 4);
+                    y = rnd1.Next(0, razm);
+                } while (Mas[x, y] != -1);
+                Mas[x, y] = i;
+                // Graph.DrawImage(image1[i], x * 90, y * 90);
+
+
+            }
+            for (int i = 0; i < razm; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                    label2.Text += Mas[j, i] + " ";
+                label2.Text += "\n";
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            image1[0] = Properties.Resources.А;
+            image1[1] = Properties.Resources.Б;
+            image1[2] = Properties.Resources.В;
+            image1[3] = Properties.Resources.Г;
+            image1[4] = Properties.Resources.Д;
+            image1[5] = Properties.Resources.Е;
+            image1[6] = Properties.Resources.Ж;
+            image1[7] = Properties.Resources.З;
+            image1[8] = Properties.Resources.И;
+            image1[9] = Properties.Resources.К;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int x = e.X / 90;
+            int y = e.Y / 90;
+            CurI = Mas[x, y];
+            Graphics Graph = pictureBox1.CreateGraphics();
+
+            if (step == 0)
+            {
+                Graph.DrawImage(image1[CurI], x * 90, y * 90);
+                step++;
+                PrevI = CurI;
+            }
+            else if (step == 1)
+            {
+                Graph.DrawImage(image1[CurI], x * 90, y * 90);
+            }
+            else if (step == 2)
+            {
+                Graph.DrawImage(image1[CurI], x * 90, y * 90);
+                if (CurI != PrevI)
+                {
+                    Graph.DrawImage(image1[CurI], x * 90, y * 90);
+                    Graph.DrawImage(image1[PrevI], x * 90, y * 90);
+                }
+            }
+            else
+
+
+                label1.Text = e.X.ToString() + " " + e.Y.ToString() + "\n" + x + "  " + y + "\n" + Mas[x, y];
+        }
+    }
+}
